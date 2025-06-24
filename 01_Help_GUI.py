b31effc1ddc96e5c590d741cf4d02fc33cc7c0b4
Tarathegnome <163245109+Tarathegnome@users.noreply.github.com>
2025-06-24 14:24:38 +1200

    Update 01_Help_GUI.py

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -7,6 +7,30 @@ import random
 class Converter:
     def __init__(self, parent):
 
+        # Formatting variables
+        background_color = "#F5CAC3"
+
+        # Converter Main Screen GUI
+        self.converter_frame = Frame(width=600, height=600, bg=background_color, pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                          font=("Arial", "20", "bold",),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+        # Help Button (row 1)
+        self.help_button = Button(self.converter_frame, text="Help",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10, command=self.help())
+        self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = Help(self)
+        get_help.help_text.configure(text="Help text goes here")
 
 # main routine
 if __name__ == "__main__":
