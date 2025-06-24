1f53674a17d1b501fc482fb65597048342d0d3ce
Tarathegnome <163245109+Tarathegnome@users.noreply.github.com>
2025-06-24 14:22:58 +1200

    Update 01_Help_GUI.py

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -1,15 +1,16 @@
 from tkinter import *
+from functools import partial   # To prevent unwanted windows
 import random
 
 
-class Convertor:
+
+class Converter:
     def __init__(self, parent):
-        print("hello world")
 
 
 # main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Convertor")
-    something = Convertor(root)
+    something = Converter(root)
     root.mainloop()
